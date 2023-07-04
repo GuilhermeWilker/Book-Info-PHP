b827d98c97de5c31585ab3409cdd589d38007151
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-07-04 13:03:51 -0300

    feat: book recommendations route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ $routes = [
 
         '/search/book/:title' => fn ($title) => load('BookController', 'searchBooks', [$title]),
         '/book/category/:category' => fn ($category) => load('BookController', 'searchByCategory', [$category]),
+        '/book/recommendations' => fn () => load('BookController', 'bookRecommendations'),
     ],
     'POST' => [],
 ];
--- src/controllers/BookController.php
+++ src/controllers/BookController.php
@@ -51,4 +51,34 @@ class BookController
 
         return $books;
     }
+
+    public function bookRecommendations()
+    {
+        $categories = ['love', 'action', 'fiction', 'science', 'filosophy', 'drama', 'adventure', 'romance', 'poem'];
+
+        $randomCategory = $categories[array_rand($categories)];
+        $url = 'https://openlibrary.org/search.json?subject='.urlencode($randomCategory);
+        $response = file_get_contents($url);
+        $data = json_decode($response, true);
+
+        $books = [];
+        $count = 0;
+
+        foreach ($data['docs'] as $doc) {
+            $book = [
+                'title' => $doc['title'],
+                'author' => isset($doc['author_name'][0]) ? $doc['author_name'][0] : 'Autor desconhecido',
+                'summary' => $doc['description'] ?? 'Resumo não fornecido',
+            ];
+
+            $books[] = $book;
+            ++$count;
+
+            if ($count >= 4) {
+                break;
+            }
+        }
+
+        return $books;
+    }
 }
